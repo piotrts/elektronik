b63aa1adfe0e58a6c13976069006fb754774c08e
Piotr Tarasewicz <tarasewiczp@gmail.com>
2017-10-31 21:03:29 +0100

    initial commit

---
+++ src/elektronik/core.cljs
@@ -0,0 +1,77 @@
+(ns elektronik.core
+  (:require [om.next :as om :refer-macros [defui]]
+            [om.dom :as dom]
+            [goog.dom :as gdom]))
+
+(enable-console-print!)
+
+(def addition-component-factory
+  #:factory{:type :math/addition
+            :name "+"
+            :desc "Addition"})
+
+(def app-state
+  {:instances/list
+   [{:instance/id 1
+     :instance/factory addition-component-factory}]})
+
+(defui Factory
+  static om/Ident
+  (ident [this props]
+    (let [{:keys [factory/type]} props]
+      [:factories/by-type type]))
+  static om/IQuery
+  (query [this]
+    [:factory/type :factory/name :factory/desc]))
+
+(defui Instance
+  static om/Ident
+  (ident [this props]
+    (let [{:keys [instance/id]} props]
+      [:instances/by-id id]))
+  static om/IQuery
+  (query [query]
+    (let [factory-query (om/get-query Factory)]
+      `[:instance/id {:instance/factory ~factory-query}]))
+  Object
+  (render [this]
+    (let [{:keys [instance/id]
+           {:keys [factory/name :factory/desc]} :instance/factory} (om/props this)]
+      (dom/div nil id ", " name ", " desc))))
+
+(def instance (om/factory Instance))
+
+(defui Root
+  static om/IQuery
+  (query [this]
+    (let [instance-query (om/get-query Instance)]
+      `[{:instances/list ~instance-query}]))
+  Object
+  (render [this]
+    (let [{:keys [instances/list]} (om/props this)]
+      (dom/div nil
+        (map instance list)))))
+
+(defmulti read om/dispatch)
+(defmulti mutate om/dispatch)
+
+(defmethod read :instances/list [{:keys [query state]} k _]
+  (let [st @state]
+    {:value (om/db->tree query (get st k) st)}))
+
+(defmethod read :default [{:keys [state]} k params]
+  (let [st @state]
+    (if-let [[_ v] (find state k)]
+      {:value v}
+      {:value :not-found})))
+
+(def parser
+  (om/parser {:read   read
+              :mutate mutate
+              :normalize true}))
+
+(def reconciler
+  (om/reconciler {:state  app-state
+                  :parser parser}))
+
+(om/add-root! reconciler Root (gdom/getElement "app"))
