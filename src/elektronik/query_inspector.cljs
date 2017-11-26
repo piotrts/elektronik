a2f415fe0a65d4db3fb1dadf6f60d7a7934e83f8
Piotr Tarasewicz <tarasewiczp@gmail.com>
2017-11-26 16:49:41 +0100

    query-inspector ui updates

--- src/elektronik/query_inspector.cljs
+++ src/elektronik/query_inspector.cljs
@@ -5,9 +5,19 @@
 
 (defui QueryInspector
   Object
+  (initLocalState [this]
+    {:expanded? true})
   (render [this]
-    (let [props (om/props this)]
-      (dom/pre nil
-        (with-out-str (pprint/pprint props))))))
+    (let [props (om/props this)
+          expanded? (om/get-state this :expanded?)]
+      (dom/div #js{:id "query-inspector"}
+        (dom/button #js{:onClick (fn [_]
+                                   (om/set-state! this {:expanded? (not expanded?)}))}
+          (if expanded?
+            "Hide"
+            "Show"))
+        (when expanded?
+          (dom/pre nil
+            (with-out-str (pprint/pprint props))))))))
 
 (def query-inspector (om/factory QueryInspector))
