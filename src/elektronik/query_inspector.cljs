48e33c523ba722de5d6d21bc4a68f86524767ce1
Piotr Tarasewicz <tarasewiczp@gmail.com>
2017-11-30 17:12:50 +0100

    fix QueryInspector

--- src/elektronik/query_inspector.cljs
+++ src/elektronik/query_inspector.cljs
@@ -6,9 +6,16 @@
 (defui QueryInspector
   Object
   (render [this]
-    (let [props (om/props this)]
+    (let [reconciler (om/get-reconciler this)
+          app-state (om/app-state reconciler)
+          parser (-> reconciler :config :parser)
+          root-query (om/get-query (om/app-root reconciler))]
       (dom/div #js{:id "query-inspector"}
+        (dom/button #js{:onClick #(.forceUpdate this)}
+          "Refresh")
         (dom/pre nil
-          (with-out-str (pprint/pprint props)))))))
+          (with-out-str
+            (pprint/pprint
+              (parser {:state app-state} root-query))))))))
 
 (def query-inspector (om/factory QueryInspector))
