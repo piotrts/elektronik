6979976ee6c064028d4647d9e1174b1fce9c1be1
Piotr Tarasewicz <tarasewiczp@gmail.com>
2017-12-07 19:03:28 +0100

    extract elektronik.transpiler

--- src/elektronik/core.cljs
+++ src/elektronik/core.cljs
@@ -112,54 +112,6 @@
     {:value {:keys [:panels/list]}
      :action #(swap! state update-in (conj ident :panel/expanded?) not)}))
 
-;; work in progress, quick and dirty
-(defn collect-instances [links]
-  (mapcat (fn [link]
-            (let [{[_ from-id] :from
-                   [_ to-id] :to} link]
-              [from-id to-id]))
-          links))
-
-(defn collect-singletons [dependency-graph]
-  (keep (fn [e]
-          (let [id (key e)
-                deps (:deps (val e))]
-            (when-not (seq deps)
-              id)))
-        dependency-graph))
-
-(defn create-dependency-graph [links]
-  (let [empty-graph (into {}
-                      (map (fn [instance]
-                             [instance {}])
-                           (collect-instances links)))]
-    (reduce (fn [graph link]
-              (let [{[_ from-id] :from
-                     [_ to-id] :to} link]
-                (update-in graph [to-id :deps] conj from-id)))
-            empty-graph
-            links)))
-
-(defn transpile-static [state]
-  (let [{links :links/list} state
-        dependency-graph (create-dependency-graph links)
-        singletons (collect-singletons dependency-graph)
-        fns (into {}
-              (map (fn [singleton]
-                     (let [factory-ident (get-in state [:instances/by-id singleton :instance/factory])
-                           factory-fn (get-in state (conj factory-ident :factory/fn))]
-                       [singleton factory-fn]))
-                   singletons))]
-    (into {}
-      (keep (fn [e]
-              (let [id (key e)
-                    deps (:deps (val e))]
-                (when (seq deps)
-                  [id (replace fns deps)])))
-            dependency-graph))))
-
-;(transpile-static @(om/app-state reconciler))
-
 (def panel-id->component
   {:query-inspector query-inspector/QueryInspector
    :state-inspector state-inspector/StateInspector})
